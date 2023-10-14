7477819550d5a522922a78109135e1bbbb6144c3
Karl Kirt <kkirtkk@gmail.com>
2023-10-14 10:50:20 +0300

    if

--- index.php
+++ index.php
@@ -1,18 +1,51 @@
 <?php
 
-$obj = new stdClass();
-$obj->name = 'Kaspar';
-$obj->nage = 30; 
-var_dump($obj);
-
-function hello() {
-    echo "hello world";
+$num = 9;
+if($num > 10){
+    var_dump('suurem');
+} else if ($num === 10) {
+    var_dump('vordne');
+} else {
+    var_dump('vaiksem');
 }
-hello();
 
-function sum($a, $b) {
-    return $a+$b;
+if($num === 1) {
+    echo 'üks';
 }
-$answer = sum(1,2);
-var_dump($answer);
-var_dump(sum(3,5));
+if($num === 2) {
+    echo 'kaks';
+}
+if($num === 3) {
+    echo 'kolm';
+}
+if($num === 4) {
+    echo 'neli';
+}
+
+// selle ülemise asemele kirjutame hoopis ...
+
+switch($num) {
+    case 1:
+        echo 'üks';
+        break;
+    case 2:
+        echo 'kaks';
+        break;
+    default: // kui pole ükski variantidest 
+        echo 'midagi muud';
+        break;
+} 
+
+for ($i=0; $i<10; $i++){
+    var_dump($i);
+}
+
+$array = [1, 2, 3, 4];
+
+foreach ($array as $value){
+    var_dump($value);
+}
+
+foreach ($array as $key=>$value){
+    var_dump($key . ':' . $value);
+}
