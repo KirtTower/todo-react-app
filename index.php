2cbe2df2aba2464d2136c5bdb63053626a1f3dbf
Karl Kirt <kkirtkk@gmail.com>
2023-10-14 09:57:53 +0300

    array

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php
 
-$bool = true;
-var_dump($bool);
-$bool = false;
+$array = ['adf', 12, true, 5];
+var_dump($array);
+var_dump($array[1]);
+$array[] = 'hello';
+var_dump($array);
+$array = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9],
+];
+var_dump($array);
