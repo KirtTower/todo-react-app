5ec403824c1f02b1bcbe60353a38e3c650a18e63
Karl Kirt <kkirtkk@gmail.com>
2023-10-14 09:23:19 +0300

    var

--- index.php
+++ index.php
@@ -1,10 +1,6 @@
 <?php
 
-$num = 1;
-echo($num);
-echo 'hello world';
-var_dump($num);
-var_dump($num);
-$string = 'sõne';
-$string = 'hello ' . $string;
-var_dump($string);
+$bool = true;
+var_dump($bool);
+$bool = false;
+var_dump(bool);
